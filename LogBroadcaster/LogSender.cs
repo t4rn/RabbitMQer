864bc4e2835449bd2777e2af1a48ba19e7537566
t4rn <tarn@onet.eu>
2017-05-30 15:42:16 +0200

    Log Broadcaster

---
+++ LogBroadcaster/LogSender.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBroadcaster
+{
+    public class LogSender
+    {
+        public static void Main(string[] args)
+        {
+            string exchangeName = "logs";
+
+            Console.WriteLine("I'm a log broadcaster...");
+
+            bool send = true;
+
+            do
+            {
+                Console.WriteLine("Enter your log message. Press [enter] to exit:");
+                string message = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(message))
+                {
+                    send = false;
+                }
+                else
+                {
+                    SendMessages(exchangeName, message);
+                }
+
+            } while (send);
+        }
+
+        private static void SendMessages(string exchangeName, string message)
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: exchangeName, type: "fanout");
+
+                    byte[] body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: exchangeName,
+                                         routingKey: "", // value ignored for fanout exchanges
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine("[x] Sent '{0}'", message);
+                }
+            }
+        }
+    }
+}
---
+++ LogReceiver/LogReceiver.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace LogReceiver
+{
+    public class LogReceiver
+    {
+        public static void Main(string[] args)
+        {
+            string exchangeName = "logs";
+
+            Console.WriteLine("I'm a log receiver...");
+
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: exchangeName, type: "fanout");
+
+                    // non-durable, exclusive, autodelete queue with a generated name
+                    string queueName = channel.QueueDeclare().QueueName;
+
+                    channel.QueueBind(queue: queueName,
+                                      exchange: "logs",
+                                      routingKey: "");
+
+                    Console.WriteLine("[*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+
+                    channel.BasicConsume(queue: queueName,
+                                         noAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine("[x] Received: '{0}'", message);
+        }
+    }
+}
